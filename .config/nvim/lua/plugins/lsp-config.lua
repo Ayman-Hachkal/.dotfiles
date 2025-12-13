c9d3a11133a950143905f02287b10595fddd8c3f
Ayman-Hachkal <ayman.hachkal@gmail.com>
2025-12-13 16:45:44 +0400

    added cmake builder as a plugin

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jdtls", "basedpyright" },
+				ensure_installed = { "lua_ls", "jdtls", "basedpyright", "glsl_analyzer"},
 			})
 		end,
 	},
@@ -18,21 +18,28 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-      lspconfig.basedpyright.setup({ 
+      -- lua_ls
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable({"lua_ls"})
+      -- java  
+      --
+      vim.lsp.config("jdtls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable({"jdtls"})
+      -- pyright
+      vim.lsp.config("basedpyright", {
         capabilities = capabilities,
         settings = {
                 basedpyright = {
+                    reportMissingTypeStubs = false,
                     analysis = {
-                        typeCheckingMode = "basic",      -- less strict type checking
+                        typeCheckingMode = "standard",      -- less strict type checking
                         diagnosticMode = "openFilesOnly",-- only report warnings in open files
-                        useLibraryCodeForTypes = false,  -- ignore types from libraries to reduce false positives
+                        useLibraryCodeForTypes = true,  -- ignore types from libraries to reduce false positives
+                        autoSearchPaths = true, -- Autolooks through venv path
                         inlayHints = {
                             variableTypes = true,
                             functionReturnTypes = true,
@@ -42,10 +49,27 @@ return {
                 },
             },
       })
+      vim.lsp.enable({"basedpyright"})
+      
+      --clangd 
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+       
+      vim.lsp.enable({"clangd"})
+
+      vim.lsp.config("glsl_analyzer", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.enable({"glsl_analyzer"})
+
+      -- keybinds
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.diagnostic.config({ virtual_text = false, virtual_text= { current_line = true }, })
+
 		end,
 	},
 }
