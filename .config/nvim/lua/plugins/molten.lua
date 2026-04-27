b51b56855b4af7de4440159ed1e194d7f9440c5e
Ayman-Hachkal <ayman.hachkal@gmail.com>
2026-04-27 15:38:07 +0100

    changed nvim

--- .config/nvim/lua/plugins/molten.lua
+++ .config/nvim/lua/plugins/molten.lua
@@ -6,5 +6,10 @@ return {
         -- this is an example, not a default. Please see the readme for more configuration options
         vim.g.molten_output_win_max_height = 12
     end,
+    "quarto-dev/quarto-nvim",
+    dependencies = {
+      "jmbuhr/otter.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    }
 }
 
